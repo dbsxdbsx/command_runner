f33573fad970c5e6974f17f2c96e97bea4332411
dbsxdbsx <djw0214@gmail.com>
2024-07-09 19:40:35 +0800

    revert the test_error.py

--- tests/test_error.py
+++ tests/test_error.py
@@ -3,6 +3,6 @@ import sys
 if __name__ == "__main__":
     for i in range(1,5):
         if i%2 == 0:
-            print(f"[{i}]:error print.", file=sys.stderr, flush=True)
+            print(f"[{i}]:error print.", file=sys.stderr)
         else:
-            print(f"[{i}]:normal print.", flush=True)
+            print(f"[{i}]:normal print.")
